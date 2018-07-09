4d021dce401b6668697eb06b28263fc6185f831e
Matias <matiasfarulla@gmail.com>
2018-07-09 23:30:29 +1200

    InfoRegalo responsive width

--- src/components/InfoRegalo/InfoRegalo.jsx
+++ src/components/InfoRegalo/InfoRegalo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const InfoRegalo = () => (
   <section className="info-regalo h-100 position-relative d-flex flex-row">
 
-    <div className="align-self-center text-center w-50 mx-auto">
+    <div className="align-self-center text-center mx-auto">
         <h3>Sobre el regalo</h3>
         <p>Un regalo no es necesario pero si nos quieren dar toda la papa, en el salón vamos a tener una caja donde pueden poner un sobre o si es más fácil una trasnferencia a la cuenta de Mati :)</p>
     </div>
