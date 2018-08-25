792e2149109d13538c6bf3980fb0dd89b303f32e
Matias <matiasfarulla@gmail.com>
2018-08-25 13:54:57 +1200

    Fixed map centering

--- src/components/ComoLlegar/ComoLlegar.jsx
+++ src/components/ComoLlegar/ComoLlegar.jsx
@@ -9,27 +9,27 @@ export default class Navigation extends React.PureComponent {
   constructor(props) {
     super(props);
 
+    this.state = {
+      center: {
+        lat: -38.1004268,
+        lng: -57.5619607,
+      },
+      zoom: 15
+    };
+
     this.getMapOptions = this.getMapOptions.bind(this);
     this.section = React.createRef();
   }
-
-  static defaultProps = {
-    center: {
-      lat: -38.1004268,
-      lng: -57.5619607,
-    },
-    zoom: 15
-  };
 
   componentDidMount() {
     this.handleScroll();
   }
 
   handleScroll() {
-    this.section.current.addEventListener('touchstart', function (e) {
-        if (e.touches.length > 1) {
-          $.fn.fullpage.setAllowScrolling(false);
-        }
+    this.section.current.addEventListener('touchstart', (e) => {
+      if (e.touches.length > 1) {
+        $.fn.fullpage.setAllowScrolling(false);
+      }
     });
     
     this.section.current.addEventListener('touchend', function (e) {
@@ -52,13 +52,14 @@ export default class Navigation extends React.PureComponent {
         <div className="map-container col p-0">
           <GoogleMapReact
             bootstrapURLKeys={{ key: 'AIzaSyAHT7kceiwbHp-uOSGG61V1X3w1dVorbQk' }}
-            defaultCenter={this.props.center}
-            defaultZoom={this.props.zoom}
+            center={this.state.center}
+            defaultZoom={this.state.zoom}
             options={this.getMapOptions}
+            resetBoundsOnResize={true}
           >
             <MarkerInfo
-              lat={this.props.center.lat}
-              lng={this.props.center.lng}
+              lat={this.state.center.lat}
+              lng={this.state.center.lng}
               image={marker}
               info="Mar Eventos (Mirador 9), Ruta 11 paraje Alfar"
             />
